20d2ad9c491bd614e237c7a50bc6009109e5b701
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-11-09 21:05:14 +0000

    async signals

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,21 @@
 import { masterElement } from "master-ts/framework/element"
 import { EMPTY_NODE, html } from "master-ts/framework/fragment"
-import { Counter } from "./counter"
+import type { Counter } from "./counter"
+
+const CounterAsync = async (props: typeof Counter['PROPS_TYPE']) => (await import("./counter")).Counter(props)
 
 export const App = masterElement('my-app', ({ $ }) => 
 {
   const toggleSignal = $.signal(false)
-  const myCounter = Counter({ startAt: 123 })
+  const myCounter = CounterAsync({ startAt: 123 })
 
   return html`
     <main>
       <h1>Master TS</h1>
 
       <h2>Counter</h2>
-      ${Counter({ startAt: 0 })}
-      <x ${Counter({ startAt: 1 })}>
-        Click me!
-      </x>
-      ${$.derive(toggleSignal, (toggle) => toggle ? myCounter : EMPTY_NODE)}
+      ${$.await(CounterAsync({ startAt: 1 }), EMPTY_NODE)}
+      ${$.derive(toggleSignal, (toggle) => toggle ? $.await(myCounter, EMPTY_NODE) : EMPTY_NODE)}
       <button on:click=${() => toggleSignal.set(!toggleSignal.value)}>Toggle</button>
     </main>
 
