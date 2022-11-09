d318abb5c8cf3759f63055360219d1446ab48088
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-11-09 19:05:49 +0000

    ...

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import { masterElement } from "master-ts/framework/element"
-import { html } from "master-ts/framework/fragment"
+import { EMPTY_NODE, html } from "master-ts/framework/fragment"
 import { Counter } from "./counter"
 
-export const App = masterElement('my-app', ({ }) => 
+export const App = masterElement('my-app', ({ $ }) => 
 {
+  const toggleSignal = $.signal(false)
+  const myCounter = Counter({ startAt: 123 })
+
   return html`
     <main>
       <h1>Master TS</h1>
@@ -13,6 +16,8 @@ export const App = masterElement('my-app', ({ }) =>
       <x ${Counter({ startAt: 1 })}>
         Click me!
       </x>
+      ${$.derive(toggleSignal, (toggle) => toggle ? myCounter : EMPTY_NODE)}
+      <button on:click=${() => toggleSignal.set(!toggleSignal.value)}>Toggle</button>
     </main>
 
     <style>
